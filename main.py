cdf14e33216d992d5dc93c627178ae14e4ef4fe7
erik <erik.grootoonk@gmail.com>
2024-11-05 22:19:51 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#print('I love Thai food')
+#print('It is spicy' )
+
+# first_name = 'Erik'
+# last_name = 'Grootje'
+# full_name = first_name +' '+last_name
+# print('Hallo ' +full_name)
+
+# age = 21 # int
+# age += 1
+
+
+# # print(type(age))    print type
+
+# print('Your age is: '+str(age))
+
+# height = 250.5  #float
+# print('Your height is: '+str(height))
+
+
+# human = False  #boolean
+# print('Are you a human? '+str(human))
+
+
+name = 'Bro'
+# print(len(name))
+# print(name.find('o'))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.replace('o','a'))
+print(name*3)
